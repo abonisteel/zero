d3b8794c3d1d6613ab1a87c339fc390bf603278a
Aster <alison.bonisteel@pcc.edu>
2020-07-29 00:08:01 -0700

    Template 1

---
+++ template/includes/footer.inc.html.php
@@ -0,0 +1,17 @@
+<footer>
+    <section class="footer">
+
+        <ul class="nav">
+            <?php include 'menu.inc.html.php' ?>
+        </ul>
+        <div class="social">
+            SOCIAL
+            <br><a href=""><img src="<?php echo BASE_URL; ?>images/facebook.png" alt="FaceBook Link"></a>
+            <a href=""><img src="<?php echo BASE_URL; ?>images/twitter.png" alt="Twitter Link"></a>
+        </div>
+        <div class="copy">Copyright &copy; 2020<br>Ace in the Hole Multisport Events<br>
+            <a href="mailto:alison.bonisteel@pcc.edu" target="_blank">alison.bonisteel@pcc.edu</a>
+        </div>
+
+    </section>
+</footer>
