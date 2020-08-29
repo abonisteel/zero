06eef07274bc335ceff4ecb4c5aaf462f01c0546
Aster <alison.bonisteel@pcc.edu>
2020-08-28 20:08:07 -0700

    Final Submission Upload

---
+++ includes/menu.inc.html.php
@@ -0,0 +1,5 @@
+<li><a href="index.php" target="_self">Home</a></li>
+<li><a href="courses.php" target="_self">Courses</a></li>
+<li><a href="register.php" target="_self">Register</a></li>
+<li><a href="faq.php" target="_self">FAQs</a></li>
+<li><a href="contact.php" target="_self">Contact</a></li>
